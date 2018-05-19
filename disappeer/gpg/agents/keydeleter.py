1bd1bef095a3d00ceb90fe673c3b42afe335c507
Disappeer Labs <disappeerlabs@gmail.com>
2018-05-19 14:39:38 -0400

    Updated key deleter to remove secret key deletion, secret key can only be deleted with passphrase in GPG but python gnupg does not yet provide interface for delete secret key with passphrase

--- disappeer/gpg/agents/keydeleter.py
+++ disappeer/gpg/agents/keydeleter.py
@@ -16,7 +16,9 @@ class KeyDeleter(gpgagent.GPGAgent):
         super().__init__(keydir)
 
     def execute(self, key_fingerprint_list):
-        prep = self.gpg.delete_keys(key_fingerprint_list, True)
+        # TODO: investigate: Delete secret key fails without passphrase
+        #           - True flag allows to delete secret key
+        #           - but python-gnupg does not provide interface for passphrase
+        # prep = self.gpg.delete_keys(key_fingerprint_list, True)
         result = self.gpg.delete_keys(key_fingerprint_list)
         return result
-
