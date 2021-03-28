b7da7d1788db9e9118b6113f875e130aab38be45
disappeer <disappeerlabs@gmail.com>
2021-03-28 10:34:14 -0400

    Added debug widget to root controller for temp work, minor refactoring

--- disappeer/root/rootcontroller.py
+++ disappeer/root/rootcontroller.py
@@ -7,20 +7,31 @@ Copyright (C) 2021 Disappeer Labs
 License: GPLv3
 """
 
-from dptools.tkcomponents.baseapp.basepanelview import BasePanelView
-from dptools.tkcomponents.baseapp.basecontroller import BaseController
+from dptools.tkcomponents.baseapp import basecontroller, basepanelview
+from dptools.tkcomponents import debugwidget
+from disappeer import metainfo
 
 
-class RootController(BaseController):
+class RootController(basecontroller.BaseController):
 
     def __init__(self, args=None, root=None):
-        BaseController.__init__(self, args=args, root=root)
+        basecontroller.BaseController.__init__(self, args=args, root=root)
         self.root = root 
         self.args = args
+        self.debug = self.add_widget_left_panel(debugwidget)
+        self.debug.click_debug_1_override(self.debug_1_override)
+        self.debug.click_debug_2_override(self.debug_2_override)
 
     def set_root_view(self):
-        return BasePanelView(self.root, title="IS THIS ON???")
+        return basepanelview.BasePanelView(self.root, title=metainfo.title)
 
     def exit(self):
         # TODO: add test to confirm quit called with exit 
         self.root.quit()
+
+    def debug_1_override(self, event):
+        print("CLICK DEBUG 1")
+
+    def debug_2_override(self, event):
+        print("CLICK DEBUG 2")
+        self.debug.append_to_textbox("HELLO THERE")
